16dfc95dc27b90af557dca4c58ed341c5a4ad78f
geronimo <gerolpz01@gmail.com>
2020-06-23 15:51:27 -0300

    Agregado servicio para leer tweets

---
+++ routers/modificarPerfil.go
@@ -0,0 +1,40 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/geronm12/twittor/bd"
+	"github.com/geronm12/twittor/models"
+)
+
+//ModificarPerfil ... Modifica el perfil del usuario
+func ModificarPerfil(w http.ResponseWriter, r *http.Request) {
+
+	var t models.Usuario
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+
+	if err != nil {
+		http.Error(w, "Datos Incorrectos "+err.Error(), 400)
+		return
+	}
+
+	var status bool
+
+	status, err = bd.ModificoRegistro(t, IDUsuario)
+
+	if err != nil {
+
+		http.Error(w, "Ocurrió un error al intentar modificar el registro. Reintente nuevamente "+err.Error(), 400)
+		return
+	}
+
+	if status == false {
+		http.Error(w, "No se ha logrado modificar el registro del usuario", 400)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+}
